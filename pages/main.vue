961144b9f1f3777c76d5f06a22e2b9c2fc290c01
Tatsuhiko <okihiutat.iutim@gmail.com>
2019-05-29 23:37:45 +0900

    add image

--- pages/main.vue
+++ pages/main.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
     <section class="hero main-bg-image"></section>
+    <img class="bg-image" src="~assets/kv.png" />
+    <!--
     <Header />
+    -->
     <section class="hero">
       <h2 class="kv-title">Page Title</h2>
     </section>
     <section class="hero main">
       <div class="columns">
-        <div class="column column-left">First column</div>
-        <div class="column column-right">Second column</div>
+        <div class="column column-left">
+          <h3 class="column-left-title">Profile</h3>
+        </div>
+        <div class="column column-right">
+          <h3>Nana</h3>
+        </div>
       </div>
       <div class="columns">
         <div class="column column-left">First column</div>
@@ -31,10 +38,10 @@
 </template>
 
 <script>
-import Header from '../components/Header.vue'
+//import Header from '../components/Header.vue'
 export default {
   components: {
-    Header
+    //Header
   },
   mounted: () => {}
 }
@@ -55,12 +62,19 @@ export default {
   background-color: #e4e1b0;
 }
 
+.bg-image {
+  box-shadow: 10px 10px 22px -13px rgba(0, 0, 0, 0.8);
+  width: 200px;
+  height: 200px;
+  z-index: 100;
+}
+
 .hero {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
   justify-content: center;
-  text-align: center;
+  //text-align: center;
   &.main {
     margin-top: 1000px;
     padding: 3% 0;
@@ -74,11 +88,23 @@ export default {
     }
 
     & .column-left {
-      background-color: #fff59d;
-      opacity: 0.8;
+      font-size: 50px;
+      color: #62ac74;
+      height: 100vh;
+      //background-color: #fff59d;
+      position: relative;
+      &-title {
+        position: absolute;
+        top: 50%;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        margin: auto;
+        text-align: center;
+      }
     }
     & .column-right {
-      margin-top: 200px;
+      height: 100vh;
       background-color: #ffffff;
       opacity: 0.8;
     }
@@ -89,6 +115,7 @@ export default {
     font-size: 30px;
     color: #62ac74;
     z-index: 10;
+    text-align: center;
   }
 }
 
